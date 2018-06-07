54650594d5d86f00970e808f77111f984a1fa033
esatgozcu <35576161+esatgozcu@users.noreply.github.com>
2018-06-07 05:53:37 +0300

    Add files via upload

---
+++ PotansiyometreIleLedKontrolu.ino
@@ -0,0 +1,21 @@
+const int analogGirisPin = A0;
+
+const int analogCikisPin = 3;
+
+int potansDeger = 0;
+
+int ledDeger = 0;
+
+void setup() {
+
+}
+
+void loop() {
+
+  potansDeger = analogRead(analogGirisPin);
+  ledDeger = map(potansDeger,0,1023,0,255);
+
+  // Örnek map (5,0,10,0,100) = 50;
+
+  analogWrite(analogCikisPin,ledDeger);
+}
